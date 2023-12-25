0f5f18c4e54d04333c4be4a269b2fc3629a4df96
Cyrille Pierre <cyrille.pierre@inrae.fr>
2023-12-25 17:47:47 +0100

    create json file for challenge1

--- scripts/generate_csv_from_world.py
+++ scripts/generate_csv_from_world.py
@@ -8,7 +8,7 @@ import xml.etree.ElementTree as ET
 MODEL_KEYWORD = 'Construction Cone'
 
 
-def extract_models_pos(world_filename):
+def extract_models_pos(world_filename, prefix):
     root = ET.parse(world_filename).getroot()
 
     coords = root.find('./world/spherical_coordinates')
@@ -22,33 +22,33 @@ def extract_models_pos(world_filename):
     models = root.findall('./world/state/model')
 
     for model in models:
-        if model.attrib['name'].startswith(MODEL_KEYWORD):
+        if model.attrib['name'].startswith(prefix):
             str_values = model.findtext('pose').split(' ')[0:3]
             pos = tuple(map(float, str_values))
             coords = enu.enu2geodetic(*pos, *anchor)
-            data.append((coords, pos))
+            data.append(list(coords) + list(pos))
             
-    return data
+    return data, anchor
 
 
 def save_csv(data, filename):
     file = open(filename, 'w')
     file.write('latitude,longitude,altitude,x,y,z\n')
 
-    for geo, pos in data:
-        file.write(f"{geo[0]},{geo[1]},{geo[2]},{pos[0]},{pos[1]},{pos[2]}\n")
+    for pt in data:
+        file.write(','.join(map(str, pt)) + '\n')
 
     file.close()
 
 
 def save_geojson(data, filename):
     features = []
-    for geo, _ in data:
+    for point in data:
         features.append({
             "type": "Feature",
             "properties": {},
             "geometry": {
-                "coordinates": [geo[1], geo[0]],
+                "coordinates": [point[1], point[0]],
                 "type": "Point"
             }
         })
@@ -59,8 +59,23 @@ def save_geojson(data, filename):
     }
 
     with open(filename, 'w') as file:
-        json.dump(root, file)
+        json.dump(root, file, indent=2)
 
+
+def save_json(data, anchor, filename):
+    root = {
+        'origin': anchor,
+        'start': data[0],
+        'end': data[1],
+        'waypoints': data[2:],
+        'fields': {
+            'vineyard': {},
+            'crops_field': {}
+        },
+    }
+
+    with open(filename, 'w') as file:
+        json.dump(root, file, indent=2)
 
 
 if __name__ == '__main__':
@@ -70,6 +85,7 @@ if __name__ == '__main__':
 
     world_filename = sys.argv[1]
     output_name = sys.argv[2]
-    data = extract_models_pos(world_filename)
+    data, anchor = extract_models_pos(world_filename, MODEL_KEYWORD)
     save_csv(data, output_name + '.csv')
     save_geojson(data, output_name + '.geojson')
+    save_json(data, anchor, output_name + '.json')
